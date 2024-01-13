30c38236c882699b39faa407af7f951db6a12003
JoaoPereira05 <jp.pereiraa@outlook.com>
2024-01-13 12:39:10 -0300

    First comit

---
+++ Array and Object Interation/solution.js
@@ -0,0 +1,36 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
+function logDairy() {
+    for (var i of dairy) {
+        console.log(i);
+    }
+}
+logDairy();
+// Task 2
+const animal = {
+
+canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (var i of Object.keys(bird)) {
+        console.log(i + ":" + " " + bird[i])
+    }
+}
+
+birdCan();
+// Task 3
+function animalCan() {
+    for (const key in bird) {
+        console.log(key + ":" + " " + bird[key])
+    }
+}
+
+animalCan();
---
+++ Writing a Unit Test/jest-testing/timesTwo.js
@@ -0,0 +1,7 @@
+// Task 1: Code the timesTwo function declaration
+function timesTwo(number) {
+    var multiplicateByTwo = number * 2;
+    return multiplicateByTwo;
+}
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
